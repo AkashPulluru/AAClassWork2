658ea35836607ff4e5beb327b692b24a57b65013
AkashPulluru <akashpulluru@gmail.com>
2023-08-22 13:38:14 -0400

    first commit

---
+++ W1D1/practice-for-ch-foundations-advanced-methods-main (2)/practice-for-ch-foundations-advanced-methods-main/06_union.rb
@@ -0,0 +1,16 @@
+# Write a method, union, that accepts any number of arrays as arguments. The
+# method should return an array containing all elements of the given arrays.
+
+def union(*arrays)
+    array = []
+    arrays.each do |subarray|
+        subarray.each do |element|
+            array << element 
+        end 
+    end 
+    return array 
+end 
+
+
+p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
+p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
