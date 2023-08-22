2e927067f837f70d29343cd4bec3fdcfa87bf859
AkashPulluru <akashpulluru@gmail.com>
2023-08-22 14:02:00 -0400

    second exercise done

--- W1D1/practice-for-ch-foundations-algorithmic-strategies-main (1)/practice-for-ch-foundations-algorithmic-strategies-main/04_peak_finder.rb
+++ W1D1/practice-for-ch-foundations-algorithmic-strategies-main (1)/practice-for-ch-foundations-algorithmic-strategies-main/04_peak_finder.rb
@@ -3,6 +3,23 @@
 # element is considered a "peak" if it is greater than both it's left and right
 # neighbor. The first or last element of the array is considered a "peak" if it
 # is greater than it's one neighbor.
+def peak_finder(arr)
+    array = [] 
+arr.each.with_index do |ele, idx|
+    if idx == 0 && arr[idx] > arr[idx+1]
+        array << ele 
+    end 
+
+    if idx == arr.length-1 && arr[idx] > arr[idx-1]
+        array << ele 
+    end 
+
+    if idx > 0 && idx < arr.length-1 && arr[idx] > arr[idx-1] && arr[idx] > arr[idx+1]
+        array << ele 
+    end 
+end 
+return array 
+end 
 
 
 p peak_finder([1, 3, 5, 4])         # => [5]
--- W1D1/practice-for-ch-foundations-algorithmic-strategies-main (1)/practice-for-ch-foundations-algorithmic-strategies-main/05_compress_str.rb
+++ W1D1/practice-for-ch-foundations-algorithmic-strategies-main (1)/practice-for-ch-foundations-algorithmic-strategies-main/05_compress_str.rb
@@ -1,6 +1,22 @@
 # Write a method, compress_str(str), that accepts a string as an arg. The method
 # should return a new str where streaks of consecutive characters are
 # compressed. For example "aaabbc" is compressed to "3a2bc".
+def compress_str(str)
+    compressed = ""
+    count = 1
+
+    (1..str.length).each do |i|
+        if str[i] == str[i - 1]
+            count += 1
+        else
+            compressed += count.to_s if count > 1
+            compressed += str[i - 1]
+            count = 1
+        end
+    end
+
+    return compressed
+end
 
 
 p compress_str("aaabbc")        # => "3a2bc"
