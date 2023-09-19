06395669ab75dcb84ee6cb7f3442cff1de34866b
Akash Pulluru <akashpulluru@gmail.com>
2023-09-18 22:34:59 -0400

    before first debugging

--- W5D1/practice-for-ch-ruby-lru-cache-main/practice-for-ch-ruby-lru-cache-main/cache.rb
+++ W5D1/practice-for-ch-ruby-lru-cache-main/practice-for-ch-ruby-lru-cache-main/cache.rb
@@ -1,17 +1,27 @@
 class LRUCache
-    def initialize
+    def initialize(arr)
+        @arr = array 
+    end
+
+    def arr
+        @arr
     end
 
     def count
       # returns number of elements currently in cache
+      return @arr.length 
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      @arr.unshift(ele)
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      @arr.each do |ele|
+        puts ele 
+      end 
     end
 
     private
